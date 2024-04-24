69df654c22a39cb2add0713d797ffb8bb3f90a57
Janosch Preuss <j.preuss@ucl.ac.uk>
2024-04-24 18:37:14 +0100

    run all scripts

---
+++ numexp/RunAll.sh
@@ -0,0 +1,7 @@
+ #!/bin/bash
+
+python3 symmetric_cavity-easy.py 
+python3 symmetric_cavity-high-contrast.py
+python3 symmetric_cavity.py
+python3 SolveMetaMaterial.py
+python3 unsymmetric-cavity.py 
